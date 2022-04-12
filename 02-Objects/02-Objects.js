122e89c0bcef19c11afae76c227e1c75c4fe9a2c
Gustavo Camarena <gustavocamarena@MacBook-Air-de-Gustavo.local>
2022-04-12 17:36:34 -0500

    Soluitions

--- 02-Objects/02-Objects.js
+++ 02-Objects/02-Objects.js
@@ -5,20 +5,22 @@ function crearClaseLibro() {
       // El constructor de la clase Libro recibe titulo (string), autor (string), traducciones (array de objetos)
       // Inicializar las propiedades del libro con los valores recibidos como argumento
       // Tu código aca:
-    
+      this.titulo = titulo;
+      this.autor = autor;
+      this.traducciones = traducciones;
       
     }
 
     getTitulo() {
       // este método debe retornar el titulo del libro.
       // Tu código aca:
-      
+      return this.titulo;
     }
 
     getAutor() {
       // El método debe retornar nombre y apellido del autor
       // Tu código aca:
-      
+      return this.autor;
     }
 
     addTraduccion(idioma, editorial) {
@@ -26,8 +28,7 @@ function crearClaseLibro() {
       // { idioma: idioma, editorial: editorial} al arreglo de traducciones del libro.
       // No debe retornar nada.
       // Tu código aca:
-           
-      
+      this.traducciones.push({idioma, editorial})
 
     }
 
@@ -37,8 +38,13 @@ function crearClaseLibro() {
       // Suponiendo que el libro tiene estas traducciones: [{idioma: 'inglés', editorial: 'Scholastic'}, {idioma: 'castellano', editorial: 'Santillana'}]
       // libro.getTraducciones() debería devolver ['inglés', 'castellano']
       // Tu código aca:
-      
+      let myArray = [];
+      this.traducciones.forEach(element => {  //forEach recorre todo el array
+        myArray.push(element.idioma)
+      });
+      return myArray;
     }
+
 
     getAlcance() {
       // El metodo debe retornar la cantidad de idiomas en la que esta traducido el libro.
@@ -47,7 +53,8 @@ function crearClaseLibro() {
       // Suponiendo que el libro tiene estas traducciones: [{idioma: 'inglés', editorial: 'Scholastic'}, {idioma: 'castellano', editorial: 'Santillana'}]
       // libro.getAlcance() deberia devolver 2
       // Tu código aca:
-      
+      let idiomas = new Set(this.getTraducciones()); // Set permite almacenar en un objeto con valores únicos
+      return idiomas.size;
     }
   }
 
@@ -72,7 +79,13 @@ const printStaff = function (objeto) {
   // Retornar un arreglo que contenga los strings indicando el titulo y nombre de cada miembro del staff
   // de esta forma "The headmaster is Albus Percival Wulfric Brian Dumbledore" 
   // el arreglo debe mantener el orden que posee el staff del objeto.
-  
+
+  let obj = Object.keys(objeto.staff);  // Objects.keys retorna un array con los nombre de las propiedades de staff
+  let myArray = [];
+  obj.forEach(element => {
+    myArray.push('The '+ element + ' is ' + objeto.staff[element].name); //concatet
+  });
+  return myArray;
 };
 
 module.exports = { crearClaseLibro, printStaff };
