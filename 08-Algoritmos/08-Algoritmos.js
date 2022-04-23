60607255ae52e9d49026ff9e458e762c5039ea18
Gustavo Camarena <gustavocamarena@MacBook-Air-de-Gustavo.local>
2022-04-22 22:27:28 -0500

    Upload Solutions

--- 08-Algoritmos/08-Algoritmos.js
+++ 08-Algoritmos/08-Algoritmos.js
@@ -5,8 +5,18 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  
- 
+  for(let i = 0; i < array.length; i++) {
+    for(let j = 0; j < array.length - 1; j++) {
+      if(array[j] > array[j+1]) {
+        let aux = array[j]
+        array[j] = array[j+1];
+        array[j+1] = aux;
+
+      }
+    }
+  }
+  console.log(array);
+  return array;
 }
 
 
@@ -15,8 +25,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-  
-
+  for(let i = 1; i < array.length; i++) {
+    for(let j = i - 1; j >= 0; j--) {
+      if(array[j] > array[j + 1]) {
+        let aux = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = aux;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -25,7 +43,21 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
- 
+  for(let i = 0; i < array.length; i ++) {
+    let minimun = array[i];
+    let current = i;
+    for(let j = i + 1; j < array.length; j++) {
+      if(array[j] < minimun) {
+        minimun = array[j];
+        current = j;
+      }
+    }
+    if(current !== i) {
+      array[current] = array[i];
+      array[i] = minimun;
+    }
+  }
+  return array;
 }
 
 
@@ -34,9 +66,36 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length <= 1) return array;
+
+  let left = [];
+  let right = [];
+  let pivot = array[0];
+
+  for(let i = 1; i < array.length; i++) {
+    if(array[i] < pivot) {
+      left.push(array[i]);
+    } else right.push(array[i]);
+  }
+  return quickSort(left).concat(pivot).concat(quickSort(right));
+
+}
+
+function merge(array1, array2) {
+  let i = 0;
+  let j = 0;
+  let result = [];
   
-
-
+  while(i < array1.length && j < array2.length) {
+    if(array1[i] < array2[j]) {
+      result.push(array1[i]);
+      i++;
+    } else {
+      result.push(array2[j]);
+      j++;
+      }
+  }
+  return result.concat(array1.slice(i)).concat(array2.slice(j));
 }
 
 function mergeSort(array) {
@@ -44,6 +103,13 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length == 1) return array;
+
+  let mid = Math.floor(array.length / 2);
+  let left = array.slice(0, mid);
+  let right = array.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
 
 }
 
